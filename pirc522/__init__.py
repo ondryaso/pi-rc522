562f65e34fb94a2ae8f14947801764f49718281f
wie-niet <diederik@wie-niet.nl>
2020-04-28 22:07:20 +0200

    Update __init__.py

--- pirc522/__init__.py
+++ pirc522/__init__.py
@@ -4,4 +4,4 @@ try:
     from .rfid import RFID
     from .util import RFIDUtil
 except RuntimeError:
-    print("Must be used on Raspberry Pi")
+    print("Must be used on Raspberry Pi or Beaglebone")
